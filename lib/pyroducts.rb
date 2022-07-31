c491ac52df090cc73bc10718ee72289fe515c4f9
Heidi Hlavinka <hchlav@gmail.com>
2022-07-31 17:37:37 -0500

    Started building CLI class, environment file, and bin file.

--- lib/pyroducts.rb
+++ lib/pyroducts.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "pyroducts/version"
+require_relative "pyroducts/cli"
+
 
 module Pyroducts
   class Error < StandardError; end
---
+++ lib/pyroducts/cli.rb
@@ -0,0 +1,10 @@
+class Pyroducts::CLI
+    def call
+        puts "Welcome to Pyroducts! We bring you facts about active volcanoes in the U.S.! Today we're visiting Hawaii."
+    end
+
+    def initialize
+        
+    end
+
+end
