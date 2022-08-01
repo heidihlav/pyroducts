6711ea7871f89373b400a9b264221607b191d83a
Heidi Hlavinka <hchlav@gmail.com>
2022-07-31 20:00:59 -0500

    Built #call in the cli class that gets data for the volcanoes and gets user input.

--- lib/pyroducts/cli.rb
+++ lib/pyroducts/cli.rb
@@ -1,10 +1,15 @@
 class Pyroducts::CLI
     def call
         puts "Welcome to Pyroducts! We bring you facts about active volcanoes in the U.S.! Today we're visiting Hawaii."
+        # get volcano name
+        # get summaries for (volcano)
+        # get facts for (volcano)
+        # list the volcano names
+        #
     end
 
-    def initialize
+    # def initialize
         
-    end
+    # end
 
 end
