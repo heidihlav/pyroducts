356a7358d812aaeb7423619da9fa6624983e1332
Heidi Hlavinka <hchlav@gmail.com>
2022-08-01 11:43:53 -0500

    Created get_summaries method to show the summary description of the volcano the user chose.

--- lib/pyroducts/cli.rb
+++ lib/pyroducts/cli.rb
@@ -12,14 +12,24 @@ class Pyroducts::CLI
     end
 
     def get_volcanoes
-        @volcanoes = Pyroducts::Volcano.all #will need @@all in volcano class
+        @volcanoes = Pyroducts::Volcano.all 
     end
+
+    #will need @@all in volcano class
 
     def list_volcano_names
         puts "Which Hawaiian volcano would you like to learn about? Type that volcano's number and press 'enter'. Type 'exit' to leave the app at any time."
         @volcanoes.each.with_index(1) do |volcano, i|
             puts "#{i}. #{volcano.name}"
         end
+    end
+
+    #will need @volcanoes instance var 
+    #- get names from scraper class
+
+    def get_summaries
+        volcano_index = gets.strip.to_i
+        show_summary_for(volcano_index) if valid_input(volcano_index, @volcanoes)
     end
 
     def goodbye
