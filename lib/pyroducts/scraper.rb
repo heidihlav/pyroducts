b5344a91ca8e1d97e19db3ce24219187910358d7
Heidi Hlavinka <hchlav@gmail.com>
2022-08-01 16:58:59 -0500

    Made Volcano class with test array to see if it works with CLI.

--- lib/pyroducts/scraper.rb
+++ lib/pyroducts/scraper.rb
@@ -1,10 +1,12 @@
-# class Pyroducts::Scraper 
-# end
+class Pyroducts::Scraper
 
-scrape_it = Nokogiri::HTML(URI.open("https://www.usgs.gov/observatories/hvo/active-volcanoes-hawaii"))
+# scrape_it = Nokogiri::HTML(URI.open("https://www.usgs.gov/observatories/hvo/active-volcanoes-hawaii"))
 
-array = scrape_it.css("p a strong").text
+# array = scrape_it.css("p a strong").text
 
-puts array
+# puts array
 
-binding.pry
+# binding.pry
+
+end
+
--- lib/pyroducts/volcano.rb
+++ lib/pyroducts/volcano.rb
@@ -1,4 +1,12 @@
 class Pyroducts::Volcano
+
+scrape_it = Nokogiri::HTML(URI.open("https://www.usgs.gov/observatories/hvo/active-volcanoes-hawaii"))
+
+array = scrape_it.css("p a strong").text
+
+puts array
+
+
 end
 
 
