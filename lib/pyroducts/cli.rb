2a755bdee5402aa5c2bf831af85351aa61b3c9c9
Heidi Hlavinka <hchlav@gmail.com>
2022-08-01 10:46:01 -0500

    Create call method and note other methods that will need to be created to make the cli function.

--- lib/pyroducts/cli.rb
+++ lib/pyroducts/cli.rb
@@ -1,12 +1,17 @@
 class Pyroducts::CLI
     def call
         puts "Welcome to Pyroducts! We bring you facts about active volcanoes in the U.S.! Today we're visiting Hawaii."
+        @input = ""
+        until @input == "exit"
         # get volcano name
+        # list the volcano names 
         # get summaries for (volcano)
         # get facts for (volcano)
-        # list the volcano names
-        #
+        end
+        goodbye 
     end
+
+    def get
 
     # def initialize
         
