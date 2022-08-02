de615d6f064ac2fd6837f70b277b46e8d48760c8
Heidi Hlavinka <hchlav@gmail.com>
2022-08-02 14:25:03 -0500

    Doing first scrape for volcano names.

--- lib/pyroducts.rb
+++ lib/pyroducts.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "pyroducts/version"
-require_relative "pyroducts/cli"
-require_relative "pyroducts/scraper"
-require_relative "pyroducts/volcano"
+require_relative "./pyroducts/version"
+require_relative "./pyroducts/cli"
+require_relative "./pyroducts/scraper"
+require_relative "./pyroducts/volcano"
 
 require 'pry'
 require 'nokogiri'
--- lib/pyroducts/scraper.rb
+++ lib/pyroducts/scraper.rb
@@ -1,11 +1,16 @@
 class Pyroducts::Scraper
 
-    doc = Nokogiri::HTML(URI.open("https://www.usgs.gov/observatories/hvo/active-volcanoes-hawaii"))
+    def self.scrape_names
+        doc = Nokogiri::HTML(open("https://www.usgs.gov/observatories/hvo/active-volcanoes-hawaii"))
+
+    # doc = Nokogiri::HTML(URI.open("https://www.usgs.gov/observatories/hvo/active-volcanoes-hawaii"))
 
     names = doc.css("p a").text
+    binding.pry
+
+    end
 
 
-binding.pry
 
 end
 
