8d7c0f4dbf64053d518bcf64cffdb65e0a9bbc5f
Heidi Hlavinka <hchlav@gmail.com>
2022-08-03 11:11:59 -0500

    test commit

--- lib/environment.rb
+++ lib/environment.rb
@@ -3,13 +3,13 @@
 
 module Pyroducts
   # puts "whyyy"
-  # class Error < StandardError; end
+  class Error < StandardError; end
   # Your code goes here...
 end
 
+require_relative ".lib/pyroducts/cli"
+require_relative "./lib/pyroducts/scraper"
 require_relative "./pyroducts/version"
-require_relative "./pyroducts/cli"
-require_relative "./pyroducts/scraper"
 require_relative "./pyroducts/volcano"
 
 require 'pry'
--- lib/pyroducts/scraper.rb
+++ lib/pyroducts/scraper.rb
@@ -1,22 +1,15 @@
 class Pyroducts::Scraper
 
-    # def self.scrape_names
-    # names = doc.css("p a").text
-    # puts names
-
-    # end
-
-    def self.scrape_descriptions
-        doc = Nokogiri::HTML(URI.open("https://www.usgs.gov/observatories/hvo/active-volcanoes-hawaii"))
-        descriptions = doc.css(".tex2jax_process p").text 
-        puts descriptions
-
-
+    def self.scrape_names
+        Nokogiri::HTML(URI.open("https://www.usgs.gov/observatories/hvo/active-volcanoes-hawaii"))
+        names = doc.css("p a").text
+        puts names
     end
 
-
-
-
+    # def self.scrape_descriptions
+    #     doc = Nokogiri::HTML(URI.open("https://www.usgs.gov/observatories/hvo/active-volcanoes-hawaii"))
+    #     descriptions = doc.css(".tex2jax_process p").text 
+    #     puts descriptions
 
 end
 
