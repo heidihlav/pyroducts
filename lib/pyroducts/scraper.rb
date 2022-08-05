0590bbade6a15b29e7d773a482e38a3d227a51a5
Heidi Hlavinka <hchlav@gmail.com>
2022-08-05 15:45:30 -0500

    Added module definition in cli, volcano, scraper class.

--- lib/pyroducts/scraper.rb
+++ lib/pyroducts/scraper.rb
@@ -1,17 +1,43 @@
-class Pyroducts::Scraper
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
+module Pyroducts
+    class Scraper
 
-    def self.scrape_names
-        Nokogiri::HTML(URI.open("https://www.usgs.gov/observatories/hvo/active-volcanoes-hawaii"))
-        names = doc.css("p a").text
-        puts names
+        def self.scrape_descriptions
+            doc = Nokogiri::HTML(URI.open("https://www.usgs.gov/volcanoes/kilauea"))
+            descriptions = doc.css("div .field-facts-block").text.split("\n") 
+            binding.pry
+            puts descriptions
+        end
+
     end
-
-    def self.scrape_descriptions
-        doc = Nokogiri::HTML(URI.open("https://www.usgs.gov/observatories/hvo/active-volcanoes-hawaii"))
-        descriptions = doc.css(".tex2jax_process p").text 
-        puts descriptions
-    end
-
 end
+
+# Pyroducts::Scraper.scrape_names  
+Pyroducts::Scraper.scrape_descriptions
+
+
+
+
+
+
+# class Pyroducts::Scraper
+
+
+#     def self.scrape_names
+#         Nokogiri::HTML(URI.open("https://www.usgs.gov/observatories/hvo/active-volcanoes-hawaii"))
+#         names = doc.css("p a").text
+#         puts names
+#     end
+
+#     def self.scrape_descriptions
+#         doc = Nokogiri::HTML(URI.open("https://www.usgs.gov/observatories/hvo/active-volcanoes-hawaii"))
+#         descriptions = doc.css(".tex2jax_process p")
+#         puts descriptions
+#         binding.pry
+#     end
+
+# end
 
--- lib/pyroducts/volcano.rb
+++ lib/pyroducts/volcano.rb
@@ -1,32 +1,33 @@
-class Pyroducts::Volcano
-    # attr_accessor :name, :location, :status, :type, :description
-    # @@all = []
+module Pyroducts
+    class Volcano
+    attr_accessor :name, :location, :status, :type, :description
+    @@all = []
 
-    # def initialize(name, location, status, type, description)
-    #     @name = name
-    #     @location = location
-    #     @status = status 
-    #     @type = type 
-    #     @description = description
-    #     save
-    # end
+    def initialize(name, location, status, type, description)
+        @name = name
+        @location = location
+        @status = status 
+        @type = type 
+        @description = description
+        save
+    end
 
-    # def self.all
-    #     @@all
-    # end
+    def self.all
+        @@all
+    end
 
-    # def get_volcano_descriptions
-    #     Pyroducts::Scraper.scrape_descriptions(self)
-    # end
+    def get_volcano_descriptions
+        Pyroducts::Scraper.scrape_descriptions(self)
+    end
 
-    # def save
-    #     @@all << self
-    # end
-
-
-
+    def save
+        @@all << self
+    end
 
 
+
+
+    end
 end
 
 
